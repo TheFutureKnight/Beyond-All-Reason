c6ffe424282ef3e2665d065694ffba28729a144f
TheFutureKnight <spidergaby1@gmail.com>
2025-06-21 04:18:37 +0300

    MMRC improvement for previous PR (Support for 90 degree)

--- luaui/Widgets/map_startbox.lua
+++ luaui/Widgets/map_startbox.lua
@@ -64,7 +64,6 @@ local amPlaced = false
 local gaiaTeamID
 
 local startTimer = Spring.GetTimer()
-local lastRot = -1 --TODO: switch this to use MiniMapRotationChanged Callin when it is added to Engine
 
 local infotextList
 
@@ -588,12 +587,6 @@ end
 
 local sec = 0
 function widget:Update(delta)
-	local currRot = getCurrentMiniMapRotationOption()
-	if lastRot ~= currRot then
-		lastRot = currRot
-		widget:ViewResize(vsx, vsy)
-		return
-	end
 	if Spring.GetGameFrame() > 1 then
 		widgetHandler:RemoveWidget()
 	end
@@ -627,4 +620,8 @@ function widget:Update(delta)
 			removeLists()
 		end
 	end
+end
+
+function widget:MiniMapRotationChanged()
+	widget:ViewResize(vsx, vsy)
 end
